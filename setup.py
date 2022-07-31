fd62170d204a63bcfda66cca47b387812242764b
Samuel Ibarra <ing.samuelibarra@gmail.com>
2022-07-31 00:19:16 -0500

    adding requirements.txt and setup py dependencies

--- setup.py
+++ setup.py
@@ -12,12 +12,18 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sibarras/sibarras-fib-py.git",
-    install_requires=[],
+    install_requires=[
+        "PyYAML>=4.1.2",
+        "dill>=0.2.8"
+    ],
+    extras_require={
+        'server': ["Flask>=1.0.0"]
+    },
     packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "Operationg System :: OS Independent",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
     test_require=['pytest'],
